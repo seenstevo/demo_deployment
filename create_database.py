347a4848d965ec40e15a38aa60431a3424ba3fdb
seenstevo <seenstevo@protonmail.com>
2023-02-28 15:23:13 +0100

    change row_id primary key

--- create_database.py
+++ create_database.py
@@ -11,14 +11,14 @@ Path('data/advertising_model_data.db').touch()
 connection = sqlite3.connect('data/advertising_model_data.db')
 c = connection.cursor()
 c.execute('''CREATE TABLE advertising
-             (row_id integer auto_increment primary key,
-              TV float NOT NULL,
-              radio float NOT NULL,
-              newspaper float NOT NULL,
-              sales float NOT NULL)''')
+             (row_id INTEGER PRIMARY KEY,
+              TV float,
+              radio float,
+              newspaper float,
+              sales float)''')
 # load the data into a Pandas DataFrame
 data = pd.read_csv('data/Advertising.csv', index_col = 0)
 # write the data to a sqlite table
-data.to_sql('advertising', connection, if_exists = 'append', index = True, index_label = 'row_id')
+data.to_sql('advertising', connection, if_exists = 'append', index = False)
 connection.commit()
 connection.close()
