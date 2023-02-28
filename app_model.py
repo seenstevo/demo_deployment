c199e35c8fc2a1f1fc4c745ae5af13d34ef77af2
seenstevo <seenstevo@protonmail.com>
2023-02-28 15:10:31 +0100

    table added add register

--- app_model.py
+++ app_model.py
@@ -11,9 +11,14 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+
+
 @app.route("/", methods=['GET'])
 def hello():
     return "Bienvenido a mi API del modelo advertising"
+
+
+
 
 
 # 1. Wndpoint que devuelva la predicción de los nuevos datos enviados mediante argumentos en la llamada
@@ -21,25 +26,28 @@ def hello():
 def predict():
     model = pickle.load(open('data/advertising_model','rb'))
 
-    tv = request.args.get('TV', None)
-    radio = request.args.get('radio', None)
-    newspaper = request.args.get('newspaper', None)
+    predict_vals = {k.lower(): v for k, v in request.args.to_dict().items()}
+    if len(predict_vals) != 3:
+        return 'This model needs 3 fields to make a prediction of sales (TV, radio and newspaper). Please review input and try again'
 
-    if tv is None or radio is None or newspaper is None:
-        return "Missing args, the input values are needed to predict"
     else:
+        tv = predict_vals['tv']
+        radio = predict_vals['radio']
+        newspaper = predict_vals['newspaper']
         prediction = model.predict([[tv,radio,newspaper]])
         return "The prediction of sales investing that amount of money in TV, radio and newspaper is: " + str(round(prediction[0],2)) + 'k €'
+
+
 
 
 # 2. To save new registers in the database
 @app.route('/v1/add_register', methods= ['GET'])
 def add_registers_db():
-    new_entry = request.args.to_dict()
-    if new_entry == {}:
-        return 'No new data to add'
+    new_entry = {k.lower(): v for k, v in request.args.to_dict().items()}
+    if len(new_entry) != 4:
+        return '4 data fields needed (TV, radio, newspaper, sales). Please check and try again'
     else:
-        tv = float(new_entry['TV'])
+        tv = float(new_entry['tv'])
         radio = float(new_entry['radio'])
         newspaper = float(new_entry['newspaper'])
         sales = float(new_entry['sales'])
@@ -49,8 +57,14 @@ def add_registers_db():
         cur.execute("INSERT INTO advertising (TV, radio, newspaper, sales) VALUES (?, ?, ?, ?)",
                     (tv, radio, newspaper, sales))
         connection.commit()
+
+        cur = connection.cursor()
+        last_rows = '''SELECT * FROM advertising ORDER BY row_id DESC LIMIT 5;'''
+        result = pd.DataFrame(cur.execute(last_rows).fetchall(), columns = ['row_id', 'TV', 'radio', 'newspaper', 'sales']).set_index('row_id')
+        result = result.to_html()
         connection.close()
-    return 'New data added successfully'
+
+    return f'New data added successfully and here are the last 5 rows:\n\n\n{result}'
 
 
 
@@ -80,4 +94,4 @@ def retrain_model():
 
 
 
-#app.run()
+app.run()  # python anywhere needs this to be commented as automatically runs this as app
--- create_database.py
+++ create_database.py
@@ -9,9 +9,16 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # crate the database
 Path('data/advertising_model_data.db').touch()
 connection = sqlite3.connect('data/advertising_model_data.db')
+c = connection.cursor()
+c.execute('''CREATE TABLE advertising
+             (row_id integer auto_increment primary key,
+              TV float NOT NULL,
+              radio float NOT NULL,
+              newspaper float NOT NULL,
+              sales float NOT NULL)''')
 # load the data into a Pandas DataFrame
 data = pd.read_csv('data/Advertising.csv', index_col = 0)
 # write the data to a sqlite table
-data.to_sql('advertising', connection, index = False)
+data.to_sql('advertising', connection, if_exists = 'append', index = True, index_label = 'row_id')
 connection.commit()
 connection.close()
